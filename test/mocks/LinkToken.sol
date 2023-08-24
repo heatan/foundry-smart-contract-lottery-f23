668c4391662ad7950bdd86c6827fabd954aaf086
heatan <axlth716@gmail.com>
2023-08-24 21:47:29 +0900

    openzeppelin

--- test/mocks/LinkToken.sol
+++ test/mocks/LinkToken.sol
@@ -4,3 +4,12 @@ pragma solidity ^0.8.0;
 
 import "@solmate/tokens/ERC20.sol";
 
+interface ERC677Receiver {
+    function onTokenTransfer(
+        address _sender,
+        uint256 _value,
+        bytes memory _data
+    ) external;
+}
+
+contract
